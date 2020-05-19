e6fd615930b6640344dc093128c04595a4082bb4
Tobet <t.betti.developer@gmail.com>
2020-05-19 23:08:26 +0200

    add dropdown.directive.ts to food app

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,15 +1,20 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
 })
 export class DropdownDirective {
 
-    @HostListener('click') onClick: Event;
+    @HostBinding('class.open') isOpen = false;
 
-    constructor(private elRef: ElementRef, private renderer2: Renderer2) {
+    constructor(private elRef: ElementRef) {
     }
 
+    // @HostListener('click') onDropdownClick() {
+    //     this.isOpen = !this.isOpen;
+    // }
 
-
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    }
 }
