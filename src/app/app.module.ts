f87c0529a9c87ecf0b58ff57a20340d5c87a8978
Tobet <t.betti.developer@gmail.com>
2020-06-04 00:06:52 +0200

    on going roting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {UsersComponent} from './users/users.component';
+import {ServersService} from './servers/servers.service';
+import {UserComponent} from './users/user/user.component';
+import {ServersComponent} from './servers/servers.component';
+import {ServerComponent} from './servers/server/server.component';
+import {EditServerComponent} from './servers/edit-server/edit-server.component';
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'users', component: UsersComponent},
+    {path: 'users/:id/:name', component: UserComponent},
+    {path: 'servers', component: ServersComponent},
+    {path: 'servers/:id', component: ServerComponent},
+    {path: 'servers/:id/edit', component: EditServerComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        UsersComponent,
+        ServersComponent,
+        UserComponent,
+        EditServerComponent,
+        ServerComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(appRoutes),
+    ],
+    providers: [ServersService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
