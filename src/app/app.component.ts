7cc0141ca91e75e0a5c5c4b098942db472cf693c
Tobet <t.betti.developer@gmail.com>
2020-09-28 23:27:41 +0200

    forms branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-udemy-course';
+
+    // different approach to get access to the form submit
+    @ViewChild('f') signupForm: NgForm;
+
+    suggestUserName() {
+        const suggestedName = 'Superuser';
+    }
+
+    // onSubmit(form: NgForm) {
+    //     console.log(form);
+    // }
+
+    onSubmit() {
+        console.log(this.signupForm);
+    }
 }
