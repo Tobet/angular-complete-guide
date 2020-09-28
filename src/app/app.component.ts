8f0f1346f0552b9c7d2ce7dcd734ed186efd6262
Tobet <t.betti.developer@gmail.com>
2020-09-28 21:16:38 +0200

    push fine modulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+import {UserService} from './user.service';
+import {Subscription} from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {}
+    userActivated = false;
+    private sub: Subscription;
+
+    constructor(private userService: UserService) {
+    }
+
+    ngOnInit() {
+        this.sub = this.userService.activatedEmitter.subscribe((didActivated: boolean) => {
+            this.userActivated = didActivated;
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
 }
