031a2f4402f88738dfde695eac015abdfb02e34d
Tobet <t.betti.developer@gmail.com>
2020-09-30 23:46:14 +0200

    td form module end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,44 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
 
+    answer = '';
+    defaultQuestion = 'pet';
+    genders = ['male', 'female'];
+
+    user = {
+        username: '',
+        email: '',
+        secretQuestion: '',
+        answer: '',
+        gender: '',
+    };
+
+    submitted = false;
+
     // different approach to get access to the form submit
     @ViewChild('f') signupForm: NgForm;
 
     suggestUserName() {
+
         const suggestedName = 'Superuser';
+
+        // bad approach
+        // this.signupForm.setValue({
+        //     userData: {
+        //         username: suggestedName,
+        //         email: '',
+        //     },
+        //     secret: 'pet',
+        //     questionAnswer: '',
+        //     gender: 'male',
+        // });
+
+        // good approach
+        this.signupForm.form.patchValue({
+            userData: {
+                username: suggestedName
+            }
+        });
     }
 
     // onSubmit(form: NgForm) {
@@ -20,6 +53,15 @@ export class AppComponent {
     // }
 
     onSubmit() {
-        console.log(this.signupForm);
+
+        this.submitted = true;
+
+        this.user.username = this.signupForm.value.userData.username;
+        this.user.email = this.signupForm.value.userData.email;
+        this.user.secretQuestion = this.signupForm.value.secret;
+        this.user.answer = this.signupForm.value.questionAnswer;
+        this.user.gender = this.signupForm.value.gender;
+
+        this.signupForm.reset();
     }
 }
