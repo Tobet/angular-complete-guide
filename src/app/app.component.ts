81552315cc3dcbd84aa96ae2bad0500378ea22dd
Tobet <t.betti.developer@gmail.com>
2020-10-06 22:37:54 +0200

    completed the assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-udemy-course';
+
+    defaultSubscription = 'Advanced';
+
+    exerciseFormData = {
+        email: null,
+        subscription: null,
+        password: null,
+    };
+
+    submitted = false;
+
+    @ViewChild('exerciseForm') exerciseForm: NgForm;
+
+    onExerciseSubmit() {
+
+        this.submitted = true;
+
+        this.exerciseFormData.email = this.exerciseForm.value.email;
+        this.exerciseFormData.subscription = this.exerciseForm.value.subscription;
+        this.exerciseFormData.password = this.exerciseForm.value.password;
+
+        this.exerciseForm.reset();
+    }
 }
