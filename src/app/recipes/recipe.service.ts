c650f61e06e4f550b74d6aa46521661c57a81ed5
Tobet <t.betti.developer@gmail.com>
2021-03-10 00:19:23 +0100

    http applied to food app

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,29 +8,36 @@ import {ShoppingListService} from '../shopping-list/shopping-list.service';
 @Injectable()
 export class RecipeService {
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'RECIPE 1',
-            'Spaghetti al ragù',
-            'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe(
-            'RECIPE 2',
-            'Soffritto di palle',
-            'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1),
-            ]
-        ),
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'RECIPE 1',
+    //         'Spaghetti al ragù',
+    //         'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe(
+    //         'RECIPE 2',
+    //         'Soffritto di palle',
+    //         'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1),
+    //         ]
+    //     ),
+    // ];
+
+    private recipes: Recipe[] = [];
 
     recipesChanged: Subject<Recipe[]> = new Subject<Recipe[]>();
 
     constructor(private shoppingListService: ShoppingListService) {
+    }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(recipes);
     }
 
     getRecipes() {
