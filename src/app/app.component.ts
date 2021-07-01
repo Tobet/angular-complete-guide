d57fd0054f0f4273a63b6cd589021c8ead4b6b64
tobet <tommasobetti91@gmail.com>
2021-07-02 01:09:03 +0200

    animations module start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'angular-udemy-course';
+    list = ['Milk', 'Sugar', 'Bread'];
+
+    onAdd(item) {
+        this.list.push(item);
+    }
 }
