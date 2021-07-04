28dc7b251740a9efc9d79b3d31b3c7f5b6f1967a
tobet <tommasobetti91@gmail.com>
2021-07-04 12:38:34 +0200

    service workers initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
-import { Post } from './post.model';
+import {Post} from './post.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  posts: Post[] = [];
+    posts: Post[] = [];
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+    }
 
-  ngOnInit() {
-    this.http
-      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
-      .subscribe(fetchedPosts => (this.posts = fetchedPosts));
-  }
+    ngOnInit() {
+        this.http
+            .get<Post[]>('https://jsonplaceholder.typicode.com/posts') // dummy rest api published
+            .subscribe(fetchedPosts => (this.posts = fetchedPosts));
+    }
 }
