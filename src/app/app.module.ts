2989ae0dc30bdd180afeae70bf4a373e742fdd85
tobet <tommasobetti91@gmail.com>
2022-11-20 00:08:13 +0100

    standalone components module initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SharedModule} from './shared/shared.module';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {DetailsComponent} from "./welcome/details/details.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, WelcomeComponent],
+    imports: [BrowserModule, SharedModule, DetailsComponent],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
