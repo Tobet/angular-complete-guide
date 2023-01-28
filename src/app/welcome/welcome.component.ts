5aa362039acb777046ee3d22baa4b983ca96a34b
tobet <tommasobetti91@gmail.com>
2023-01-29 00:24:22 +0100

    migrated highlight.directive.ts to standalone world

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,8 +1,6 @@
 import {Component} from '@angular/core';
-import {DetailsComponent} from "./details/details.component";
 
 @Component({
-    // imports: [DetailsComponent],
     selector: 'app-welcome',
     templateUrl: './welcome.component.html'
 })
